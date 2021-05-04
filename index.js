869c7f91e43e42e6ed0eb2bb35e8fdcc2207fab7
Kyle <kjfossman@gmail.com>
2021-05-04 10:02:24 -0800

    commit tree d44ead6eac5344659252f370914fb04b9eb7db37

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+for(let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+
+function wrapGifts(gifts){
+for(let i = 0; i < gifts.length; i++){
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+}
+    return gifts;
+}
+
+function writeCards(names, event){
+    let cards = []
+    for(let i = 0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+       return cards
+}
+
+function countDown(x) {
+    while(x >= 0) {
+        console.log(x--);
+    }
+
+}
